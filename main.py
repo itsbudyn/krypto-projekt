c0dccfbf6f55677a1af27aad053ed7b42956fb98
itsbudyn <jakub.kedziora@gmail.com>
2022-05-30 16:15:47 +0200

    basic shared key generation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
+parameters = dh.generate_parameters(generator=2, key_size=512)
+
 class Party:
     def __init__(self,identity):
         self.identity=identity
@@ -14,3 +16,26 @@ class Party:
 
     def generateSharedKey(self):
         self.shared_key=self.private_key.exchange(self.opponent_public_key)
+
+    def __str__(self):
+        string=f"""INFORMACJE O {self.identity}
+        KLUCZ PRYWATNY: \t{self.private_key}
+        KLUCZ PUBLICZNY:\t{self.public_key}
+        
+        KLUCZ WSPÓŁDZIELONY:\t{self.shared_key}
+        
+        KLUCZ PUB. PRZECIWNY:\t{self.opponent_public_key}"""
+        return string
+
+def exchangePublicKeys(peer_a:Party,peer_b:Party):
+    peer_a.opponent_public_key=peer_b.public_key
+    peer_b.opponent_public_key=peer_a.public_key
+
+A=Party("A")
+B=Party("B")
+
+exchangePublicKeys(A,B)
+A.generateSharedKey()
+B.generateSharedKey()
+
+print(A.shared_key==B.shared_key)
