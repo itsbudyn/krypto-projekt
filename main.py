6b2e4ce9750481cbe291ca656f2d44d940706290
itsbudyn <jakub.kedziora@gmail.com>
2022-05-30 20:21:50 +0200

    now with MAC and MAC verification

--- main.py
+++ main.py
@@ -1,48 +1,69 @@
-from base64 import encode
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import dh, ed25519
+from cryptography.hazmat.primitives import hashes, serialization, hmac
+from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 parameters = dh.generate_parameters(generator=2, key_size=512)
 
 class Party:
+    # konstruktor
     def __init__(self,identity):
         self.identity=identity
+        self.identity_mac=None
+
         self.private_key=parameters.generate_private_key()
         self.public_key=self.private_key.public_key()
 
         self.shared_key=None
-        self.derived_key=None
 
         self.opponent_public_key=None
         self.opponent_identity=None
         self.opponent_identity_mac=None
 
+    # Jeżeli mamy klucz publiczny przeciwnika, generujemy z niego i z naszego prywatnego klucz współdzielony
     def generateSharedKey(self):
         self.shared_key=self.private_key.exchange(self.opponent_public_key)
 
-    def __str__(self):
-        string=f"""INFORMACJE O {self.identity}
-        KLUCZ PRYWATNY: \t{self.private_key}
-        KLUCZ PUBLICZNY:\t{self.public_key}
-        
-        KLUCZ WSPÓŁDZIELONY:\t{self.shared_key}
-        
-        KLUCZ PUB. PRZECIWNY:\t{self.opponent_public_key}"""
-        return string
+    # Generuje swój MAC ze swojej tożsamości
+    def generateIdentityMAC(self):
+        h=hmac.HMAC(self.shared_key,hashes.SHA256())
+        h.update(bytearray(self.identity.encode()))
+        self.identity_mac=h.finalize()
 
+    # Wylicza MAC z danych przeciwnika, i porównuje z odebranym MAC-iem
+    def verifyOpposingMAC(self):
+        h=hmac.HMAC(self.shared_key,hashes.SHA256())
+        h.update(bytes(self.opponent_identity.encode()))
+        result=h.finalize()
+        print("Zgodność MAC wygenerowanego lokalnie z odebranym:",self.opponent_identity_mac==result)
+
+# Instancjowanie obiektów klasy Party jako partie A i B o podanych tożsamościach
 A=Party("Alice")
 B=Party("Bob")
 
-# KROK 1 = A WYSYŁA g^x DO B
-B.opponent_public_key=A.public_key
+# KROK 1 = A wysyła do B
+B.opponent_public_key = A.public_key    # Swój klucz publiczny
 
-# KROK 2
-A.opponent_public_key=B.public_key
-A.opponent_identity=B.identity
+# KROK 2 = B wysyła do A
+A.opponent_public_key = B.public_key    # Swój klucz publiczny
+A.opponent_identity   = B.identity      # Swoją tożsamość (surową)
 
-A.generateSharedKey()
-B.generateSharedKey()
+A.generateSharedKey()                   # Ponieważ obie strony mają klucze publiczne przeciwnika i swoje prywatne
+B.generateSharedKey()                   # wygenerują na podstawie nich klucz współdzielony
 
-print(A.shared_key==B.shared_key)
-print(A.derived_key==B.derived_key)
+B.generateIdentityMAC()                 # B wygeneruje MAC swojej tożsamości z klucza współdzielonego
+A.generateIdentityMAC()                 # A wygeneruje MAC swojej tożsamości z klucza współdzielonego (nie jest potrzebne jeszcze w tym kroku)
+
+A.opponent_identity_mac=B.identity_mac  # B wyśle do A swój MAC z tożsamości
+print("A sprawdzi teraz MAC z B")
+A.verifyOpposingMAC()                   # A sam wyliczy MAC i zweryfikuje z odebranym
+
+# KROK 3 = A wysyła do B
+B.opponent_identity     = A.identity       # Swoją tożsamość (surową)
+B.opponent_identity_mac = A.identity_mac   # Swój MAC ze swojej tożsamości
+print("B sprawdzi teraz MAC z A")
+B.verifyOpposingMAC()
+
+# serializacja kluczy - to robiłem jak się siłowałem z podpisami cyfrowymi
+A_private_key_serialized=A.private_key.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.NoEncryption())
+
+print("Zgodność współdzielonych kluczy:",A.shared_key==B.shared_key)
