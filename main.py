8e0c1cfb203d6c8ba1948c7ca4c6b0de1446e205
itsbudyn <jakub.kedziora@gmail.com>
2022-05-30 17:35:59 +0200

    now generating derived key + prep for signing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import dh
+from base64 import encode
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import dh, ed25519
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 parameters = dh.generate_parameters(generator=2, key_size=512)
@@ -11,6 +12,7 @@ class Party:
         self.public_key=self.private_key.public_key()
 
         self.shared_key=None
+        self.derived_key=None
 
         self.opponent_public_key=None
         self.opponent_identity=None
@@ -18,6 +20,14 @@ class Party:
 
     def generateSharedKey(self):
         self.shared_key=self.private_key.exchange(self.opponent_public_key)
+
+    def generateDerivedKey(self):
+        self.derived_key=HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=None,
+            info=b'handshake data'
+        ).derive(self.shared_key)
 
     def __str__(self):
         string=f"""INFORMACJE O {self.identity}
@@ -39,4 +49,11 @@ B.opponent_public_key=A.public_key
 A.opponent_public_key=B.public_key
 A.opponent_identity=B.identity
 
+A.generateSharedKey()
+B.generateSharedKey()
+
+A.generateDerivedKey()
+B.generateDerivedKey()
+
 print(A.shared_key==B.shared_key)
+print(A.derived_key==B.derived_key)
